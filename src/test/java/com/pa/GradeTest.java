afab447d72e78bd9e9f096f9f300b4b3c664dc10
lxn-git <1846066322@qq.com>
2019-07-19 22:38:11 +0800

    台式机修改

--- src/test/java/com/pa/GradeTest.java
+++ src/test/java/com/pa/GradeTest.java
@@ -3,11 +3,7 @@ package com.pa;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
-
-import static org.testng.Assert.*;
 
 public class GradeTest {
     @BeforeClass
