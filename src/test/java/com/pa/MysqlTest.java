95c237a857b8ec26b23d6072c5b655a4faac2152
lxn-git <1846066322@qq.com>
2019-07-20 23:09:24 +0800

    22

--- src/test/java/com/pa/MysqlTest.java
+++ src/test/java/com/pa/MysqlTest.java
@@ -8,7 +8,7 @@ public class MysqlTest {
     @Test
     public  void getAssertData() {
         MySqlDr mySqlDr = new MySqlDr();
-        String result = mySqlDr.getData("select bill_nm FROM tbl_chacc_brand_grade_inf WHERE seq_id=0","bill_nm");//查询语句
+        String result = mySqlDr.getData("select * FROM tbl_chacc_brand_grade_inf WHERE seq_id=0","bill_nm");//查询语句
         System.out.println(result);
         Assert.assertEquals(result,"优惠券");
     }
